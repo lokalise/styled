7a1d5c333b2bb4e36537e73774e3adba549a1733
Arturs Vonda <github@artursvonda.lv>
2023-02-27 11:14:30 +0200

    chore: adjust docs and package

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ export default defineConfig({
 	build: {
 		lib: {
 			entry: resolve(__dirname, "src/index.ts"),
-			name: "YourProjectName",
-			fileName: "your-project-name",
+			name: "styled",
+			fileName: "index",
 		},
 	},
 	test: {
