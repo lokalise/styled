8bce4a0c3d2453a11d4c4d273cd984bfaa290b09
Arturs Vonda <arturs.vonda@lokalise.com>
2023-03-08 11:45:27 +0200

    feat: add `valueFromProp` and media query helper

--- src/tokens/index.ts
+++ src/tokens/index.ts
@@ -1,4 +1,5 @@
 export { excludeProps } from "./excludeProps";
 export { generateHelpers } from "./generateHelpers";
 export { modifier } from "./modifier";
+export { valueFromProp } from "./valueFromProp";
 export { variants } from "./variants";
